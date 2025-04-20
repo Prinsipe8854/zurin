289cc97db91e3c7c5cafba63d53269b7e53e2041
zurin <49125408+Prinsipe8854@users.noreply.github.com>
2025-04-20 23:36:59 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'd4b7f8e2567c6138c206e47fdebec1b6';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/w500';
     let currentItem;
